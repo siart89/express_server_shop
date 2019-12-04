68d2f4e63b27c192124779e5c7703465a8e12d69
siart89 <artyomreva@gmail.com>
2019-12-04 20:57:05 +0300

    changed mechanics of an avatar adding

--- src/components/profile/profileElements/Avatar.js
+++ src/components/profile/profileElements/Avatar.js
@@ -1,27 +1,22 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
 import {
   AvatarBox,
   AddInp,
   InputLabel,
-  Tip,
-  SubmitBtn,
 } from '../profileStyles/styles';
 import setUrl from '../../../store/actions/setUrl';
 
 
 const Avatar = ({ name }) => {
-  const [tips, setTips] = useState('');
-  const [file, setFile] = useState(null);
   const url = useSelector((state) => state.isAuth.avatar);
   const dispatch = useDispatch();
 
   const handleSendData = async (e) => {
-    e.preventDefault();
     const data = new FormData();
 
-    data.append('avatar', file);
+    data.append('avatar', e.target.files[0]);
     const resp = await fetch('/profile/avatar', {
       method: 'POST',
       headers: {
@@ -32,34 +27,22 @@ const Avatar = ({ name }) => {
     const result = await resp.json();
 
     dispatch(setUrl(result.url));
-    setFile(null);
   };
 
   return (
     <AvatarBox url={url}>
-      <InputLabel isOnIn={tips} htmlFor="avatar">
+      <InputLabel htmlFor="avatar">
         {!url && name[0].toUpperCase()}
       </InputLabel>
       <form
         style={{ position: 'relative' }}
-        onSubmit={handleSendData}
-        onMouseLeave={() => setTips(false)}
-        onMouseOverCapture={() => setTips(true)}
       >
         <AddInp
           type="file"
           name="avatar"
           id="avatar"
-          onChange={(e) => setFile(e.target.files[0])}
+          onChange={handleSendData}
         />
-        {tips && <Tip>Добавить изображение</Tip>}
-        {file && (
-          <SubmitBtn
-            type="submit"
-          >
-            +
-          </SubmitBtn>
-        )}
       </form>
     </AvatarBox>
 
