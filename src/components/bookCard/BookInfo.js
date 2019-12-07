8f1549ca17d25a155d24b707aed45d251e228f13
siart89 <artyomreva@gmail.com>
2019-12-07 21:31:06 +0300

    added proptypes

--- src/components/bookCard/BookInfo.js
+++ src/components/bookCard/BookInfo.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import {
   BookInfoWrapper,
   BookTitle,
@@ -8,23 +9,33 @@ import {
   CoverImg,
 } from './bookCardStyles';
 
-const BookInfo = ({title, author, cover, description}) => {
-  return (
-    <BookInfoWrapper>
-      <BookTitle>
-        {title}
-      </BookTitle>
-      <BookStyledText>
-        {author}
-      </BookStyledText>
-      <BookBigCoverBox>
-        <CoverImg src={cover}/>
-      </BookBigCoverBox>
-      <BookMainText>
-        {description}
-      </BookMainText>
-    </BookInfoWrapper>
-  );
+const BookInfo = ({
+  title,
+  author,
+  cover,
+  description,
+}) => (
+  <BookInfoWrapper>
+    <BookTitle>
+      {title}
+    </BookTitle>
+    <BookStyledText>
+      {author}
+    </BookStyledText>
+    <BookBigCoverBox>
+      <CoverImg src={cover} />
+    </BookBigCoverBox>
+    <BookMainText>
+      {description}
+    </BookMainText>
+  </BookInfoWrapper>
+);
+
+BookInfo.propTypes = {
+  title: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
+  cover: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
 };
 
 export default BookInfo;
