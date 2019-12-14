902013e844974201f7d0ce5374db398324e2a6ff
Papika-Dev <revatele@gmail.com>
2019-12-14 13:12:37 +0300

    added another check

--- server/components/authorization.js
+++ server/components/authorization.js
@@ -2,23 +2,34 @@ import fs from 'fs';
 import jwt from 'jsonwebtoken';
 import db from './db';
 import { makeNewSession } from './authentication';
-import { resolve } from 'dns';
+
 
 const secretKey = fs.readFileSync('./server/secret/secret.key', 'utf8');
 
+const chekInSessions = async (id, token) => {
+  const isExist = await db.one(`SELECT (user_id, refresh_token) FROM sessions
+   WHERE user_id = $1 AND refresh_token = $2;`, [id, token]);
+   if (isExist) return true;
+   return false;
+}
+
 const checkToken = async (req, res, next) => {
   if (typeof req.headers.authorization !== 'undefined') {
-    let info = [];
     const token = req.headers.authorization.split(' ')[1];
     const refreshToken = req.headers.authorization.split(' ')[2];
     jwt.verify(token, secretKey, { algorithm: 'HS256' }, (err, encoded) => {
       if (err) {
-        jwt.verify(refreshToken, secretKey, { algorithm: 'HS256' }, (err, encoded) => {
+        
+        jwt.verify(refreshToken, secretKey, { algorithm: 'HS256' }, async (err, encoded) => {
           const { ip, id, name, os } = encoded;
-          req.id = encoded.id;
-          if (ip === req.ip && os === req.useragent.os) {
-            makeNewSession(req, next, name, id)
-          }
+          if (await chekInSessions(id, refreshToken)) {
+            req.id = encoded.id;
+            if (ip === req.ip && os === req.useragent.os) {
+              makeNewSession(req, next, name, id)
+            }
+          } else {
+            res.sendStatus(403);
+          }     
         })
       } else {
         req.id = encoded.id;
