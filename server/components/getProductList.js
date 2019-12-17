d7c9d3edd0988d0cd6f863c1052af62f3f62e241
siart89 <artyomreva@gmail.com>
2019-12-17 18:11:25 +0300

    delete live search

--- server/components/getProductList.js
+++ server/components/getProductList.js
@@ -1,8 +1,4 @@
-import bodyParser from 'body-parser';
-import { as } from 'pg-promise';
 import db from './db';
-
-const jsonParser = bodyParser.json();
 
 export default (app) => {
   app.get('/products/all', async (req, res) => {
@@ -12,21 +8,5 @@ export default (app) => {
     } catch (e) {
       res.sendStatus(500);
     }
-  });
-
-  app.post('/live/search/', jsonParser, async (req, res) => {
-    db.task(async (t) => {
-      const text = as.value(req.body.find);
-      const titles = await t.any(`SELECT title FROM books 
-        WHERE title ILIKE '$1:value%'`, [text]);
-      const authors = await t.any(`SELECT author FROM books 
-        WHERE author ILIKE '$1:value%'`, [text]);
-      return {
-        titles,
-        authors,
-      };
-    })
-      .then((data) => console.log(data))
-      .catch((e) => console.log(e));
   });
 };
